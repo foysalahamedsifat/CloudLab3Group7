ef350ed236a465e7ce5cdab3b2d310e7b4fc78b1
foysalahamedsifat <foysalahamedsifat@gmail.com>
2024-10-31 10:03:56 -0400

    Initial Commit

---
+++ Lab3_Group_7/Lab3_Group_7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab3_Group_7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
